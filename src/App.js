188bac6bbab424f0cfcb9032ee6f64586d6c4e63
Smith, Justin (PMF) <smithj@arrowmaker.com>
2020-06-26 14:52:47 -0400

    add color form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react"
+import AddColorForm from "./components/AddColorForm"
 import ColorData from "./data/color-data.json"
 import ColorList from "./components/ColorList"
+import { v4 } from "uuid"
 
 export default function App() {
   const [colors, setColors] = useState(ColorData)
   return (
-    <ColorList
-      colors={colors}
-      onRateColor={(id, rating) => {
-        const newColors = colors.map(color =>
-          color.id === id ? { ...color, rating } : color
-        )
-        setColors(newColors)
-      }}
-      onRemoveColor={id => {
-        const newColors = colors.filter(color => color.id !== id)
-        setColors(newColors)
-      }}
-    />
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [...colors, { id: v4(), rating: 0, title, color }]
+          setColors(newColors)
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? { ...color, rating } : color
+          )
+          setColors(newColors)
+        }}
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id)
+          setColors(newColors)
+        }}
+      />
+    </>
   )
 }
